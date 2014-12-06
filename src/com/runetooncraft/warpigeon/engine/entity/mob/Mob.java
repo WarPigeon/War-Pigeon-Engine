f111b2414ed6651bc37693d9668d99ede100a15e
Deery50 <marcus@marcusman.com>
2014-12-05 22:29:05 -0500

    creating new sliding collision system...currently very very glitchy.

--- src/com/runetooncraft/warpigeon/engine/entity/mob/Mob.java
+++ src/com/runetooncraft/warpigeon/engine/entity/mob/Mob.java
@@ -3,6 +3,7 @@ package com.runetooncraft.warpigeon.engine.entity.mob;
 import com.runetooncraft.warpigeon.engine.entity.Entity;
 import com.runetooncraft.warpigeon.engine.graphics.Sprite;
 import com.runetooncraft.warpigeon.engine.level.CollisionType;
+import com.runetooncraft.warpigeon.engine.level.Tile;
 import com.runetooncraft.warpigeon.engine.level.TileCoordinate;
 
 public abstract class Mob extends Entity {
@@ -21,6 +22,7 @@ public abstract class Mob extends Entity {
 	public int xas,yas;
 	protected boolean Sideways = false;
 	public int layerPresent = 1;
+	private int lastsideofTile = 0;
 	
 	//Don't worry about this unless the game is a sidescroller
 	public int weight = 0;
@@ -44,7 +46,6 @@ public abstract class Mob extends Entity {
 	}
 	
 	public void moveNoAnimate(int xa, int ya) {
-		boolean Collide = true;
 		if(xa != 0 && ya != 0) {
 			move(xa,0);
 			move(0,ya);
@@ -58,9 +59,13 @@ public abstract class Mob extends Entity {
 		if (xa > 0) dir = 3;
 		if (ya < 0) dir = 2;
 		if (ya > 0) dir = 0;
-		if (!collision(xa,ya)) {
-			Collide = false;
+		int sideofTile = collision(xa,ya);
+		if (sideofTile == -1) {
 			x += xa;
+			y += ya;
+		} else if (sideofTile == 0 || sideofTile == 2)  {
+			x += xa;
+		} else if (sideofTile == 1 || sideofTile == 3) {
 			y += ya;
 		}
 	}
@@ -90,11 +95,18 @@ public abstract class Mob extends Entity {
 		if (ya < 0 && xa < 0) dir = 7;
 //		System.out.println("Dir: " + dir);
 		
-		if (!collision(xa,ya)) {
-			Collide = false;
-			x += xa;
-			y += ya;
-		}
+			int sideofTile = collision(xa,ya);
+			if (sideofTile == -1) {
+				x += xa;
+				y += ya;
+				Collide = false;
+			} else if (sideofTile == 0 || sideofTile == 2)  {
+				x += xa;
+				Collide = false;
+			} else if (sideofTile == 1 || sideofTile == 3) {
+				y += ya;
+				Collide = false;
+			}
 		
 		if(Collide == false) {
 			if(dir != LastDir) {
@@ -147,7 +159,7 @@ public abstract class Mob extends Entity {
 			sprite = RightAnims[0];
 		}
 	}
-	public boolean collision(int xa, int ya) {
+	public boolean checksolidBelow(int xa, int ya) {
 		boolean solid = false;
 		
 		for(int i = 0; i < 4; i++) {
@@ -156,19 +168,104 @@ public abstract class Mob extends Entity {
 				if (level.colltype.equals(CollisionType.BASIC)) {
 					if (level.getTileLayer(level.getLayer(layerPresent), xp, yp).collide(i)) solid = true;
 				} else if(level.colltype.equals(CollisionType.ADVANCED_COLLBOX)) {
+					
 					if (level.getTileLayerCollision(level.getCollisionLayer(layerPresent), xp, yp) != null) {
-						if (level.getTileLayerCollision(level.getCollisionLayer(layerPresent), xp, yp).collide(i)) solid = true;
+						Tile t = level.getTileLayerCollision(level.getCollisionLayer(layerPresent), xp, yp);
+						if (t.collide(i)) {
+							solid = true;
+						}
+						
 					} else {
 						System.out.println("Nulled tile in Collision" + layerPresent + " at " + xp + "," + yp);
 					}
 				}
 			}
-		
 		return solid;
 	}
 	
+	public int collision(int xa, int ya) {
+		boolean solid = false;
+		boolean sendforceColl = false;
+		int sideofTile = -1;
+		double xposTile = 0.0;
+		double yposTile = 0.0;
+		
+		for(int i = 0; i < 4; i++) {
+				int xp = ((x + xa) + i % 2 * 12 - 7) / engine.getScreenEngine2D().PixelWidth;
+				int yp = ((y + ya) + i / 2 * 12 + 7) / engine.getScreenEngine2D().PixelHeight;
+				if (level.colltype.equals(CollisionType.BASIC)) {
+					if (level.getTileLayer(level.getLayer(layerPresent), xp, yp).collide(i)) solid = true;
+				} else if(level.colltype.equals(CollisionType.ADVANCED_COLLBOX)) {
+					
+					if (level.getTileLayerCollision(level.getCollisionLayer(layerPresent), xp, yp) != null) {
+						Tile t = level.getTileLayerCollision(level.getCollisionLayer(layerPresent), xp, yp);
+						if (t.collide(i)) {
+							xposTile = (((double)x)/engine.getScreenEngine2D().PixelWidth);
+							yposTile = (((double)y)/engine.getScreenEngine2D().PixelHeight);
+							// 0 - north| 1- east | 2 - south | 3 - west |
+							
+							if(xa == 0) {
+								sendforceColl = true;
+								if(yposTile < yp) {
+									sideofTile = 0;
+								} else {
+									sideofTile = 2;
+								}
+							} else if (ya == 0) {
+								sendforceColl = true;
+								if(xposTile < xp) {
+									sideofTile = 1;
+								} else {
+									sideofTile = 3;
+								}
+								
+							} else {
+								if(xposTile < xp) {
+									if(yposTile < yp) {
+										sideofTile = lastsideofTile;
+									} else {
+										sideofTile = 3;
+									}
+								} else if(xposTile > xp) {
+									if(yposTile > yp) {
+										sideofTile = lastsideofTile;
+									} else {
+										sideofTile = 1;
+									}
+								} else if (yposTile < yp) {
+									if(xposTile < xp) {
+										sideofTile = lastsideofTile;
+									} else {
+										sideofTile = 0;
+									}
+								} else if (yposTile > yp) {
+									if(xposTile > xp) {
+										sideofTile = lastsideofTile;
+									} else {
+										sideofTile = 2;
+									}
+								}
+							}
+							
+							solid = true;
+						}
+						
+					} else {
+						System.out.println("Nulled tile in Collision" + layerPresent + " at " + xp + "," + yp);
+					}
+				}
+			}
+		lastsideofTile = sideofTile;
+		System.out.println("xa: " + xa + " ya: " + ya + " side: " + sideofTile);
+		if(sendforceColl) {
+			return -2;
+		} else {
+			return sideofTile;
+		}
+	}
+	
 	public boolean checkCollideBelow() {
-		return collision(xas, (yas - engine.getScreenEngine2D().PixelHeight));
+		return checksolidBelow(xas, (yas - engine.getScreenEngine2D().PixelHeight));
 	}
 	
 	public Sprite getSprite() {
